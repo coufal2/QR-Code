61921637efc1059cd6c9923d3b37bb691a3109b9
coufal2 <134766618+coufal2@users.noreply.github.com>
2024-09-27 16:14:32 +0200

    Update register.php

--- register.php
+++ register.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="cs">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, content="initial-scale=1.0">
     <title>Registrace</title>
     <link rel="stylesheet" href="style.css">
 </head>
